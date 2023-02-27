0709f84a548a481e42f411e24f1b15521a434e19
cuonglexuan203 <cuonglexuan203@gmail.com>
2023-02-27 14:12:51 +0700

    first upload

---
+++ ShapeMovingApp/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace ShapeMovingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TimerShape = new System.Windows.Forms.Timer(this.components);
+            this.PnlMainDrawing = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // TimerShape
+            // 
+            this.TimerShape.Tick += new System.EventHandler(this.TimerShape_Tick);
+            // 
+            // PnlMainDrawing
+            // 
+            this.PnlMainDrawing.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PnlMainDrawing.Location = new System.Drawing.Point(0, 0);
+            this.PnlMainDrawing.Name = "PnlMainDrawing";
+            this.PnlMainDrawing.Size = new System.Drawing.Size(800, 450);
+            this.PnlMainDrawing.TabIndex = 0;
+            this.PnlMainDrawing.Paint += new System.Windows.Forms.PaintEventHandler(this.PnlMainDrawing_Paint);
+            this.PnlMainDrawing.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.PnlMainDrawing_PreviewKeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PnlMainDrawing);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer TimerShape;
+        private System.Windows.Forms.Panel PnlMainDrawing;
+    }
+}
+
---
+++ ShapeMovingApp/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShapeMovingApp
+{
+    public partial class Form1 : Form
+    {
+        SolidBrush brush;
+        Color color = Color.Blue;
+        Point point;
+        int step = 5;
+        int size = 50;
+        bool isForward = true;
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            brush = new SolidBrush(color);
+            point = new Point(100, 100);
+            this.TimerShape.Interval = 200;
+            this.TimerShape.Start();
+
+        }
+
+        private void PnlMainDrawing_Paint(object sender, PaintEventArgs e)
+        {
+            brush.Color = color;
+            e.Graphics.FillEllipse(brush, point.X, point.Y, size, size);
+            if (this.point.X + size >= this.PnlMainDrawing.Width)
+            {
+                isForward = false;
+            }
+            if(this.point.X <= 0)
+            {
+                isForward = true;
+            }
+            this.PnlMainDrawing.Focus();
+        }
+
+        private void TimerShape_Tick(object sender, EventArgs e)
+        {
+            if (isForward)
+            {
+                TimerShape_Forward(step);
+            }
+            else
+            {
+                TimerShape_Backward(step);
+            }
+            this.PnlMainDrawing.Refresh();
+
+        }
+        private void TimerShape_Forward(int offset)
+        {
+            point.X += offset;
+        }
+        private void TimerShape_Backward(int offset)
+        {
+            point.X -= offset;
+        }
+
+        private void PnlMainDrawing_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Up:
+                    {
+                        this.point.Y -= 5;
+                        break;
+                    }
+                case Keys.Down:
+                    {
+                        this.point.Y += 5;
+                        break;
+                    }
+                case Keys.Left:
+                    {
+                        this.point.X -= 5;
+                        isForward = false;
+                        break;
+                    }
+                case Keys.Right:
+                    {
+                        this.point.X += 5;
+                        isForward = true;
+                        break;
+                    }
+                case Keys.L:
+                    {
+                        this.size = 100;
+                        break;
+                    }
+                case Keys.S:
+                    {
+                        this.size = 50;
+                        break;
+                    }
+                case Keys.C:
+                    {
+                        this.color = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0,255));
+                        break;
+                    }
+            }
+
+            this.PnlMainDrawing.Refresh();
+        }
+    }
+}
